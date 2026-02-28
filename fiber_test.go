4b8573fc313ad8e40769d01f6fa866c9cff24881
arzaqulmughny <zaarza03@gmail.com>
2026-02-28 22:02:31 +0700

    test: Add `TestGetQuery` for validating query parameter extraction and move app initialization to package level.

--- fiber_test.go
+++ fiber_test.go
@@ -9,9 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRoutingHello(t *testing.T) {
-	app := fiber.New()
+var app = fiber.New()
 
+func TestRoutingHello(t *testing.T) {
 	app.Get("/", func(ctx fiber.Ctx) error {
 		return ctx.SendString("Hello")
 	})
@@ -25,4 +25,41 @@ func TestRoutingHello(t *testing.T) {
 	bytes, err := io.ReadAll(response.Body)
 
 	assert.Equal(t, "Hello", string(bytes))
+}
+
+func TestGetQuery(t *testing.T) {
+	app.Get("/hello", func(ctx fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+		return ctx.SendString("Hello " + name)
+	})
+
+	// Test send query
+	request := httptest.NewRequest("GET", "/hello?name=Arza", nil)
+	response, err := app.Test(request)
+
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello Arza", string(bytes))
+
+	// Test send without query, should return fallback value from name query
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err = io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello Guest", string(bytes))
 }
